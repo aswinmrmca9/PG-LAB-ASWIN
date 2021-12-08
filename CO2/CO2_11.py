ed02db00c50a9c2996064f907414b19a3fa85502
aswinmrmca9 <93874365+aswinmrmca9@users.noreply.github.com>
2021-12-08 15:36:10 +0530

    Add files via upload

---
+++ CO2/CO2_11.py
@@ -0,0 +1,7 @@
+import math
+ta = lambda b,h:1/2*b*h 
+ra = lambda l,b:l*b 
+sa = lambda a:a*a
+print("Triangle Area:",ta(10,20)) 
+print("Rectangle Area:",ra(30,20)) 
+print("Square Area :",sa(15))
