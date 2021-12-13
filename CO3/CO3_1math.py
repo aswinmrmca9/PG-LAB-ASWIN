f62098430bc2bd21a8dc0ce04ee2f443305c7949
aswinmrmca9 <93874365+aswinmrmca9@users.noreply.github.com>
2021-12-13 11:48:50 +0530

    Add files via upload

---
+++ CO3/CO3_1math.py
@@ -0,0 +1,11 @@
+import math
+print("The value of pi ",math.pi)
+
+import math as m
+print("The value of pi ",m.pi)
+
+from math import pi,sqrt
+print(math.pi)
+print(math.sqrt(2))
+print(math.tan(90))
+print(math.cos(30))
---
+++ CO3/CO3_2time.py
@@ -0,0 +1,12 @@
+import time
+print("current time in sec",time.time())
+print("current time",time.ctime())
+print("current time after 30 s",time.ctime(time.time()+30))
+t=time.localtime()
+print("time ",t)
+print("current year",t.tm_year)
+print("current month",t.tm_mon)
+print("current day",t.tm_mday)
+print("current hour",t.tm_hour)
+print("current week",t.tm_wday)
+print("day of year",t.tm_yday)
