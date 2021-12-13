12f03dd0b2b104dfa53438075dafe468182854cb
aswinmrmca9 <93874365+aswinmrmca9@users.noreply.github.com>
2021-12-13 12:42:24 +0530

    Add files via upload

---
+++ CO3/CO3_satist.py
@@ -0,0 +1,6 @@
+import statistics as s
+print("harmonic_mean ",s.harmonic_mean([20,30,40]))
+print("mean ",s.mean([9,10,24,35,36]))
+print("median ",s.median([9,10,24,35,36]))
+print("mode ",s.mode([9,10,24,35,36]))
+print("variance ",s.variance([9,10,24,35,36]))
